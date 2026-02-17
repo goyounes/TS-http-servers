0a097321705c571005143b58c3c3c268b9b813bd
goyounes <Younes.sahar.ing@gmail.com>
2026-02-17 12:36:25 +0100

    updated to use express.json middleware

--- src/handlers/handlerValidateChirp.ts
+++ src/handlers/handlerValidateChirp.ts
@@ -2,33 +2,19 @@ import { Request, Response } from "express";
 import { config } from "../config.js"
 
 export async function handlerValidateChirp(req: Request, res: Response) {
-  let body = ""; // 1. Initialize
 
-  // 2. Listen for data events
-  req.on("data", (chunk) => {
-    body += chunk;
-  });
-
-  // 3. Listen for end events
-  req.on("end", () => {
-    try {
-        const parsedBody = JSON.parse(body);
-        // now you can use `parsedBody` as a JavaScript object
-        if (!parsedBody.body){
-            res.status(400).send(JSON.stringify({"error": "Something went wrong"}))
-            return
-        }
-
-        if (parsedBody.body.length > 140){
-            res.status(400).send(JSON.stringify({"error": "Chirp is too long"}))
-            return
-        }
-
-        res.status(200).send(JSON.stringify({"valid": true}))
-
-    }   catch (error) {
-      res.status(400).send("Invalid JSON");
+    const parsedBody = req.body;
+    // now you can use `parsedBody` as a JavaScript object
+    if (!parsedBody.body){
+        res.status(400).send(JSON.stringify({"error": "Something went wrong"}))
+        return
     }
-  });
+
+    if (parsedBody.body.length > 140){
+        res.status(400).send(JSON.stringify({"error": "Chirp is too long"}))
+        return
+    }
+
+    res.status(200).send(JSON.stringify({"valid": true}))
 
 }
--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express()
 const PORT = 8080
 
 
-
+app.use(express.json());
 app.use(middlewareLogResponses);
 app.use("/app",middlewareMetricsInc);
 app.use("/app", express.static("./src/app"));
