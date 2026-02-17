0f1ecbbd4b54dd2783e1e5ef93df1e0ef0ad68cb
goyounes <Younes.sahar.ing@gmail.com>
2026-02-17 13:02:03 +0100

    added body text validation

--- src/handlers/handlerValidateChirp.ts
+++ src/handlers/handlerValidateChirp.ts
@@ -2,19 +2,34 @@ import { Request, Response } from "express";
 import { config } from "../config.js"
 
 export async function handlerValidateChirp(req: Request, res: Response) {
-
-    const parsedBody = req.body;
+    type parameters = {
+        body: string;
+    };
+    const params: parameters = req.body;
     // now you can use `parsedBody` as a JavaScript object
-    if (!parsedBody.body){
+    if (!params.body){
         res.status(400).send(JSON.stringify({"error": "Something went wrong"}))
         return
     }
 
-    if (parsedBody.body.length > 140){
+    if (params.body.length > 140){
         res.status(400).send(JSON.stringify({"error": "Chirp is too long"}))
         return
     }
 
-    res.status(200).send(JSON.stringify({"valid": true}))
+    const cleanText = replaceProfaneWords(params.body)
+    res.status(200).send(JSON.stringify({"cleanedBody": cleanText}))
 
+}
+
+function replaceProfaneWords (text: string){
+    console.log(text)
+    const words = text.split(" ")
+    console.log(words)
+    for (let i = 0; i < words.length; i++){
+        if (words[i].toLowerCase() === "kerfuffle") words[i] = "****"
+        if (words[i].toLowerCase() === "sharbert") words[i] = "****"
+        if (words[i].toLowerCase() === "fornax") words[i] = "****"
+    }
+    return words.join(" ")
 }
