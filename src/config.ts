e6d3e042ba1ad751424b99c2c3b9dd64e72ab680
goyounes <Younes.sahar.ing@gmail.com>
2026-02-18 07:37:07 +0100

    first migration - Users table

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,18 @@
+process.loadEnvFile()
+function envOrThrow (key: string): string {
+    if (!process.env.dbURL) throw new Error("database url is not set") 
+
+    return process.env.dbURL
+}
+
 export type APIConfig = {
   fileserverHits: number;
+  dbURL: string
 };
 
 export const config: APIConfig = {
-    fileserverHits: 0
+    fileserverHits: 0,
+    dbURL:  envOrThrow("dbURL")
 }
+
 
