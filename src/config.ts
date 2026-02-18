09785688d549c83c91cd559cd53ed9c1600cfe63
goyounes <Younes.sahar.ing@gmail.com>
2026-02-18 07:58:05 +0100

    added automatic migrations

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,35 @@
 process.loadEnvFile()
 function envOrThrow (key: string): string {
     if (!process.env.dbURL) throw new Error("database url is not set") 
-
     return process.env.dbURL
 }
 
 export type APIConfig = {
   fileserverHits: number;
-  dbURL: string
 };
-
-export const config: APIConfig = {
+const apiConfig = {
     fileserverHits: 0,
-    dbURL:  envOrThrow("dbURL")
 }
 
+export type DBConfig = {
+  url: string,
+  migrationConfig: MigrationConfig
+};
 import type { MigrationConfig } from "drizzle-orm/migrator";
-
 const migrationConfig: MigrationConfig = {
   migrationsFolder: "./src/db/migrations",
 };
+const dbConfig = {
+  url: envOrThrow("dbURL"),
+  migrationConfig: migrationConfig
+};
+
+export type Config = {
+    api: APIConfig;
+    db: DBConfig;
+}
+export const config: Config = {
+    api: apiConfig,
+    db: dbConfig
+}
 
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+// ------ init automatic migration ------
+import postgres from "postgres";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import { drizzle } from "drizzle-orm/postgres-js";
+import {config } from "./config.js"
+const migrationClient = postgres(config.db.url, { max: 1 });
+await migrate(drizzle(migrationClient), config.db.migrationConfig);
+// ------ init express app ------
+
 import express, { NextFunction, Response, Request } from "express"
 import { handlerReadiness } from "./handlers/handlerReadiness.js";
 import { middlewareLogResponses } from "./middlewares/logResponses.js";
