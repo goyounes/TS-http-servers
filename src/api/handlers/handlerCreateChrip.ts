ed80c57f4271a0f5fb18acf26817ab23082fb16f
goyounes <Younes.sahar.ing@gmail.com>
2026-02-24 21:05:56 +0100

    added createChirp handler and chirps queries

---
+++ src/api/handlers/handlerCreateChrip.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from "express";
+import { respondWithError, respondWithJSON } from "../json.js";
+import { BadRequestError } from "../middlewares/errorsClasses.js";
+import { Chirp, NewChirp } from "../../db/schema.js";
+import { createChirp } from "../../db/queries/chirps.js";
+
+export async function handlerCreateChrip(req:Request, res: Response){
+    type parameters = {
+        body: string;
+        userId: string;
+    };
+    const params: parameters = req.body;
+
+    if (!params.body){
+        throw new BadRequestError("JSON Invalid format")
+    }
+
+    const MAX_CHRIP_LENGTH = 140;
+    if (params.body.length > MAX_CHRIP_LENGTH){
+        throw new BadRequestError(`Chirp is too long. Max length is ${MAX_CHRIP_LENGTH}`)
+    }
+
+    const cleanText = replaceProfaneWords(params.body)
+
+    const newChirp : NewChirp = {
+        body: cleanText,
+        userId: params.userId //|| "9c5d6f6f-4f15-4b86-9408-4d7fefe556f3"
+    } 
+    console.log(newChirp)
+
+try {
+    const chirp : Chirp = await createChirp(newChirp)
+    if (!chirp) {
+        // throw new Error("Could not create chirp");
+    }
+    respondWithJSON(res, 201, chirp)
+
+} catch (error: any) {
+    console.log(error?.message || error)
+    respondWithJSON(res, 201, "in error block")
+
+}
+
+
+
+}
+
+function replaceProfaneWords (text: string){
+    const words = text.split(" ")
+    for (let i = 0; i < words.length; i++){
+        if (words[i].toLowerCase() === "kerfuffle") words[i] = "****"
+        if (words[i].toLowerCase() === "sharbert") words[i] = "****"
+        if (words[i].toLowerCase() === "fornax") words[i] = "****"
+    }
+    return words.join(" ")
+}
