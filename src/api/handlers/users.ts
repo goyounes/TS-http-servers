d94c8519442db9468beb29e3068afd46de9468f7
goyounes <Younes.sahar.ing@gmail.com>
2026-02-25 21:22:39 +0100

    improved register function and added login function

--- src/api/handlers/users.ts
+++ src/api/handlers/users.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
-import { createUser } from "../../db/queries/users.js";
+import { createUser, getUserByEmail } from "../../db/queries/users.js";
 import { NewUser, User, UserResponse } from "../../db/schema.js";
-import { BadRequestError } from "../middlewares/errorsClasses.js";
+import { BadRequestError, UserNotAuthenticatedError } from "../middlewares/errorsClasses.js";
 import { respondWithJSON } from "../json.js";
-import { hashPasword } from "../auth.js";
+import { checkPasswordHash, hashPassword } from "../auth.js";
 
 export async function handlerRegister(req:Request, res: Response){
     type parameters = {
@@ -16,19 +16,52 @@ export async function handlerRegister(req:Request, res: Response){
         throw new BadRequestError("Missing required fields");
     }
 
-    const hash = await hashPasword(params.password)
+    const hash = await hashPassword(params.password)
 
     const newUser: NewUser = {
         email: params.email,
         hashedPassword: hash
     }
 
-    const user: UserResponse = await createUser(newUser)
-    
-    if (!user) {
+    const userResponse: UserResponse = await createUser(newUser)
+
+    if (!userResponse) {
         throw new Error("Could not create user");
     }
 
-    respondWithJSON(res, 201, user)
+    respondWithJSON(res, 201, userResponse)
+
+}
+
+export async function handlerLogin(req:Request, res: Response){
+    type parameters = {
+        email: string;
+        password: string;
+    };
+    const params: parameters = req.body;
+
+    if(!params.email || !params.password){
+        throw new BadRequestError("Missing required fields");
+    }
+    
+    const user : User = await getUserByEmail(params.email )
+    if (!user){
+        throw new UserNotAuthenticatedError(`incorrect email or password`)
+    }
+
+    const correctPassword = await checkPasswordHash(params.password, user.hashedPassword)
+
+    if (!correctPassword) {
+        throw new UserNotAuthenticatedError(`incorrect email or password`);
+    }
+
+    const userResponse: UserResponse = {
+        id: user.id,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+        email: user.email
+    }
+
+    respondWithJSON(res, 200, userResponse)
 
 }
