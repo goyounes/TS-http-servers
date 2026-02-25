ac70bc109259c3233ee75df29c5526fef6cd1f6d
goyounes <Younes.sahar.ing@gmail.com>
2026-02-25 21:22:46 +0100

    added login route

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ import { middlewareLogResponses } from "./api/middlewares/logResponses.js";
 import { middlewareMetricsInc } from "./api/middlewares/metricsInc.js";
 import { handlerMetrics } from "./api/handlers/handlerMetrics.js";
 import { handlerResetMetrics } from "./api/handlers/handlerResetMetrics.js";
-import { handlerRegister } from "./api/handlers/users.js";
+import { handlerLogin, handlerRegister } from "./api/handlers/users.js";
 import { errorMiddleware } from "./api/middlewares/errors.js";
 import { asyncHandler } from "./api/handlers/asyncHandler.js";
 import { handlerCreateChrip, handlerGetChirp, handlerGetChirps } from "./api/handlers/chirps.js";
@@ -32,6 +32,7 @@ app.get("/admin/metrics", asyncHandler(handlerMetrics));
 app.post("/admin/reset", asyncHandler(handlerResetMetrics));
 
 app.post("/api/users", asyncHandler(handlerRegister) )
+app.post("/api/login", asyncHandler(handlerLogin) )
 
 app.get("/api/chirps", asyncHandler(handlerGetChirps) )
 app.get("/api/chirps/:id", asyncHandler(handlerGetChirp) )
