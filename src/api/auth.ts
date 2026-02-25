238fd62086a8f39aa1ef9faf1c480e1541a33d33
goyounes <Younes.sahar.ing@gmail.com>
2026-02-25 21:22:04 +0100

    simplified auth.ts

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,22 +1,9 @@
 import argon2 from "argon2"
 
-export async function hashPasword(password: string): Promise<string>{
-    try {
-        const hash = await argon2.hash(password);
-        return hash
-    } catch (err) {
-        throw new Error("failed to hash password")
-    }
+export async function hashPassword(password: string): Promise<string>{
+    return argon2.hash(password);
 }
 
 export async function checkPasswordHash(password: string, hash: string): Promise<boolean>{
-    try {
-        if (await argon2.verify(hash, password)) {
-            return true
-        } else {
-            return false
-        }
-    } catch (err) {
-        throw new Error("failed to check password")
-    }
+    return argon2.verify(hash, password);
 }
