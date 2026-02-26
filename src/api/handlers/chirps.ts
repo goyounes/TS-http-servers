56289a634ef47ad368dd2dc597367f044fa8f6b6
goyounes <Younes.sahar.ing@gmail.com>
2026-02-26 11:52:32 +0100

    refactored validateChirp

--- src/api/handlers/chirps.ts
+++ src/api/handlers/chirps.ts
@@ -4,37 +4,7 @@ import { BadRequestError, NotFoundError } from "../middlewares/errorsClasses.js"
 import { Chirp, chirps, NewChirp } from "../../db/schema.js";
 import { createChirp, getAllChirps, getChirp } from "../../db/queries/chirps.js";
 
-export async function handlerCreateChrip(req:Request, res: Response){
-    type parameters = {
-        body: string;
-        userId: string;
-    };
-    const params: parameters = req.body;
-
-    if (!params.body){
-        throw new BadRequestError("JSON Invalid format")
-    }
-
-    const MAX_CHRIP_LENGTH = 140;
-    if (params.body.length > MAX_CHRIP_LENGTH){
-        throw new BadRequestError(`Chirp is too long. Max length is ${MAX_CHRIP_LENGTH}`)
-    }
-
-    const cleanText = replaceProfaneWords(params.body)
-
-    const newChirp : NewChirp = {
-        body: cleanText,
-        userId: params.userId //|| "9c5d6f6f-4f15-4b86-9408-4d7fefe556f3"
-    } 
-    console.log(newChirp)
-
-    const chirp : Chirp = await createChirp(newChirp)
-    if (!chirp) {
-        throw new Error("Could not create chirp");
-    }
-    respondWithJSON(res, 201, chirp)
-
-}
+const MAX_CHIRP_LENGTH = 140;
 
 function replaceProfaneWords (text: string){
     const words = text.split(" ")
@@ -44,6 +14,37 @@ function replaceProfaneWords (text: string){
         if (words[i].toLowerCase() === "fornax") words[i] = "****"
     }
     return words.join(" ")
+}
+
+function validateChirp(body: string): string {
+    if (!body) {
+        throw new BadRequestError("JSON Invalid format");
+    }
+    if (body.length > MAX_CHIRP_LENGTH) {
+        throw new BadRequestError(`Chirp is too long. Max length is ${MAX_CHIRP_LENGTH}`);
+    }
+    return replaceProfaneWords(body);
+}
+
+export async function handlerCreateChrip(req: Request, res: Response) {
+    type parameters = {
+        body: string;
+        userId: string;
+    };
+    const params: parameters = req.body;
+
+    const cleanBody = validateChirp(params.body);
+
+    const newChirp: NewChirp = {
+        body: cleanBody,
+        userId: params.userId,
+    };
+
+    const chirp: Chirp = await createChirp(newChirp);
+    if (!chirp) {
+        throw new Error("Could not create chirp");
+    }
+    respondWithJSON(res, 201, chirp);
 }
 
 
